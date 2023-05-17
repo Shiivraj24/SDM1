7f94b6d9a0c598f06d771a5fc304dfdfb10e2d82
dac <dac@dypm.org>
2023-05-17 12:44:43 +0530

    exam files

---
+++ db.sql
@@ -0,0 +1,7 @@
+create table User(user_id int primary key,usre_name varchar(20),user_mobno varchar(20),user_dob date);
+
+create table Employer (emp_id int,emp_name varchar(30),org_id int);
+
+create table job(job_id int,job_details varchar(20),org_id int);
+
+create table interview (interview_id int,interview_date date);
